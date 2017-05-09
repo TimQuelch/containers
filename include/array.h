3d2d460e2d3624a8984bbc05cb2f54a74493cd85
Tim Quelch <tim@quelch.name>
2017-05-09 19:30:35 +1000

    Rework array to use modern C++
    - New data member names
    - make_unique

--- include/array.h
+++ include/array.h
@@ -7,19 +7,19 @@ namespace containers {
 	template <class T> class array {
 	public:
 		array(size_t size = 0)
-		    : m_size(size)
-		    , m_data(size ? new T[size] : nullptr) {}
+		    : size_{size}
+		    , data_{size ? std::make_unique<T[]>(size) : nullptr} {}
 
 		array(const T& other)
-		    : m_size(other.m_size)
-		    , m_data(m_size ? new T[other.m_size] : nullptr) {
-			std::copy(std::begin(other.m_data), std::end(other.m_data), std::begin(m_data));
+		    : size_{other.size_}
+		    , data_{size_ ? std::make_unique<T[]>(other.size_) : nullptr} {
+			std::copy(std::begin(other.data_), std::end(other.data_), std::begin(data_));
 		}
 
 		array(T&& other)
-		    : m_size(other.m_size)
-		    , m_data(std::move(other.m_data)) {
-			other.m_data = nullptr;
+		    : size_{other.size_}
+		    , data_{std::move(other.data_)} {
+			other.data_ = nullptr;
 		}
 
 		array& operator=(array other) {
@@ -29,11 +29,12 @@ namespace containers {
 
 		~array() = default;
 
-		T& operator[](size_t index) { return *(m_data.get() + index); }
+		T& operator[](size_t index) { return *(data_.get() + index); }
+		size_t size() { return size_; }
 
 	private:
-		size_t m_size;
-		std::unique_ptr<T> m_data;
+		size_t size_;
+		std::unique_ptr<T[]> data_;
 	};
 }
 
