b0f1d001ae7b1f63a229650ed8c6250470f9ef8f
Tim Quelch <tim@quelch.name>
2017-05-10 10:06:29 +1000

    Add additional tests for array

--- test/array_test.cpp
+++ test/array_test.cpp
@@ -35,3 +35,55 @@ BOOST_AUTO_TEST_CASE(elementAccessAt) {
 		BOOST_TEST(a.at(i) == i);
 	}
 }
+
+BOOST_AUTO_TEST_CASE(elementAccessFront) {
+	size_t size = 5;
+	containers::array<int> a{size};
+	a[0] = 5;
+	BOOST_TEST(a.front() == 5);
+	a.front() = 7;
+	BOOST_TEST(a.front() == 7);
+}
+
+BOOST_AUTO_TEST_CASE(elementAccessBack) {
+	size_t size = 5;
+	containers::array<int> a{size};
+	a[size - 1] = 5;
+	BOOST_TEST(a.back() == 5);
+	a.back() = 7;
+	BOOST_TEST(a.back() == 7);
+}
+
+BOOST_AUTO_TEST_CASE(getEmpty) {
+	containers::array<int> a{static_cast<size_t>(0)};
+	containers::array<int> b{static_cast<size_t>(5)};
+	BOOST_TEST(a.empty());
+	BOOST_TEST(!b.empty());
+}
+
+BOOST_AUTO_TEST_CASE(fillArray) {
+	size_t size = 5;
+	int val = 3;
+	containers::array<int> a{size};
+	a.fill(val);
+	for (int i = 0; i < size; i++) {
+		BOOST_TEST(a[i] == val);
+	}
+	val = -4;
+	a.fill(val);
+	for (int i = 0; i < size; i++) {
+		BOOST_TEST(a[i] == val);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(getSize) {
+	size_t sizeA = 5;
+	containers::array<int> a{sizeA};
+	BOOST_TEST(a.size() == sizeA);
+	size_t sizeB = 2;
+	containers::array<int> b{sizeB};
+	BOOST_TEST(b.size() == sizeB);
+	size_t sizeC = 8;
+	containers::array<int> c{sizeC};
+	BOOST_TEST(c.size() == sizeC);
+}
